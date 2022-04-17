02c51e19612de231527047d13e525ec5735516aa
Vladislav <Vladislav_Shupikov@epam.com>
2022-04-17 17:09:47 +0200

    Initial setup

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    bracketSpacing: false,
+    jsxBracketSameLine: true,
+    singleQuote: true,
+    trailingComma: 'all',
+    arrowParens: 'always',
+    semi: true,
+    printWidth: 100,
+    bracketSpacing: true,
+    tabWidth: 4,
+  };
+  
