8315ccbfb5c43150ad2d681d8f382048501e897e
Vladislav <Vladislav_Shupikov@epam.com>
2022-04-17 20:56:36 +0200

    Adds react testing library

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -1,14 +1,11 @@
-/**
- * @format
- */
-
 import 'react-native';
 import React from 'react';
-import App from '../App';
+import App from '../app/App';
+import { render } from '@testing-library/react-native';
 
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+jest.mock('../app/screens/MainScreen', () => 'MainScreen');
 
 it('renders correctly', () => {
-  renderer.create(<App />);
+    const { getByTestId } = render(<App />);
+    expect(getByTestId('AppRoot')).toBeDefined();
 });
