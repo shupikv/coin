940224375b0966aa778511d088f33bb4c76a5d9c
Vladislav <Vladislav_Shupikov@epam.com>
2022-04-18 11:33:08 +0200

    Disables Eslint for js files

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
   extends: '@react-native-community',
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
+  ignorePatterns: ["*.js"],
   overrides: [
     {
       files: ['*.ts', '*.tsx'],
